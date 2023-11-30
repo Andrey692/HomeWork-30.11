4443e5c5c979beaf22dffb6ae5895cb1f1833d6c
Andrey <bot6923777@outlook.com>
2023-11-30 11:53:10 +0300

    Initial commit

---
+++ task.2/Program.cs
@@ -0,0 +1,56 @@
+﻿
+// задайте массив на 10 целых чисел.
+// Определить колличество четных чисел в массиве.
+
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index<length)
+    {
+        collection[index] = new Random().Next(1,10); //   10 не входит
+        index++;
+    }
+}
+
+void PrintArray(int[] collection)
+{
+    // int length = collection.Length;
+    // int index = 0;
+    // while (index<length)
+    // {
+    //     System.Console.Write(collection[index] + " ");
+    //     index++;
+    // }
+            System.Console.Write("["+ string.Join(", ",collection)+"] " + "=> ");
+
+}
+
+
+int  IndexOf(int[] collection)
+{
+    int count = collection.Length;
+    int index = 0;
+    int scor = 0;   // подсчет четных элементов
+
+    while (index<count)
+    {
+        if (collection[index] % 2 == 0)
+        {
+            scor = scor + 1;
+        }
+        index++;
+    }
+    return scor;
+}
+
+int[] array = new int[10];  // создаем новый массив в котором 10 элементов
+
+FillArray(array);  // вызов функции заполнения
+
+PrintArray(array); // вызов функции вывода
+
+
+int pos = IndexOf(array);
+System.Console.WriteLine(pos);
+
